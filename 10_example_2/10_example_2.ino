93ed5b4385b928958ec0987bb1756c87d89a02f1
meowtivator <02dosiu@gmail.com>
2025-10-11 21:37:51 +0900

    Add Code

---
+++ 10_example_2/10_example_2.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+#define PIN_SERVO 10
+
+Servo myservo;
+
+void setup() {
+  myservo.attach(PIN_SERVO); 
+  // 0 == 6
+  // 90 == 105
+  myservo.write(0);
+  delay(1000);
+}
+
+void loop() {
+    // add code here.
+//    myservo.write(0);
+//    delay(500);
+//    myservo.write(90);
+//    delay(500);
+//    myservo.write(180);
+//    delay(500);
+//    myservo.write(90);
+//    delay(500);
+//    myservo.write(0);
+//    delay(500);
+}
